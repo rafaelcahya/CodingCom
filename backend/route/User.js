30c5092b4472233b83811dc7f326e9a950375d64
willi <willi@DESKTOP-MVOPDLN>
2021-04-19 11:24:45 +0700

    update

--- backend/route/User.js
+++ backend/route/User.js
@@ -4,49 +4,78 @@ const router = express.Router()
 
 const db = require('../config/db')
 
+const nodemailer = require('nodemailer')
+
 router.post("/register", (req, res) => {
     const fullname = req.body.fullname
     const name = req.body.name
     const email = req.body.email
     const password = req.body.password
     const confirmpassword = req.body.confirmpassword
-    
-    if(fullname.length<=0){
-        res.send({message:"Fullname can not be empty"})
-    }else if(name.length <=0){
-        res.send({message:"Username can not be empty"})
-    }else if(name.length >=32){
-        res.send({message:"Username must be less than 32 character"})
-    }else if(name.match(/[ ]/)!=null){
-        res.send({message:"Username must not has space"})
-    }else if(email.length<=0){
-        res.send({message:"Email can not be empty"})
-    }else if(email.match(/[@]/)==null){
-        res.send({message:"Email is invalid"})
-    }else if(email.match(/[.]/)==null){
-        res.send({message:"Email is invalid"})
-    }else if(password <=0){
-        res.send({message:"Password can not be empty"})
-    }else if(password.length < 8){
-        res.send({message:"Password must be at least 8 character"})
-    }else if(password.length >= 20){
-        res.send({message:"Password must be less than 20 character"})
-    }else if(password.match(/[A-Z]/) == null){
-        res.send({message:"Password must has 1 uppercase"})
-    }else if(password.match(/[a-z]/) == null){
-        res.send({message:"Password must has 1 lowecase"})
-    }else if(password.match(/[0-9]/) == null){
-        res.send({message:"Password must has 1 number"})
-    }else if(confirmpassword<=0){
-        res.send({message:"Confirm password can not be empty"})
-    }else if(confirmpassword != password){
-        res.send({message:"Confirm password must be same as password"})
+
+    if (fullname.length <= 0) {
+        res.send({ message: "Fullname can not be empty" })
+    } else if (name.length <= 0) {
+        res.send({ message: "Username can not be empty" })
+    } else if (name.length >= 32) {
+        res.send({ message: "Username must be less than 32 character" })
+    } else if (name.match(/[ ]/) != null) {
+        res.send({ message: "Username must not has space" })
+    } else if (email.length <= 0) {
+        res.send({ message: "Email can not be empty" })
+    } else if (email.match(/[@]/) == null) {
+        res.send({ message: "Email is invalid" })
+    } else if (email.match(/[.]/) == null) {
+        res.send({ message: "Email is invalid" })
+    } else if (password <= 0) {
+        res.send({ message: "Password can not be empty" })
+    } else if (password.length < 8) {
+        res.send({ message: "Password must be at least 8 character" })
+    } else if (password.length >= 20) {
+        res.send({ message: "Password must be less than 20 character" })
+    } else if (password.match(/[A-Z]/) == null) {
+        res.send({ message: "Password must has 1 uppercase" })
+    } else if (password.match(/[a-z]/) == null) {
+        res.send({ message: "Password must has 1 lowecase" })
+    } else if (password.match(/[0-9]/) == null) {
+        res.send({ message: "Password must has 1 number" })
+    } else if (confirmpassword <= 0) {
+        res.send({ message: "Confirm password can not be empty" })
+    } else if (confirmpassword != password) {
+        res.send({ message: "Confirm password must be same as password" })
     }else {
-        
-        db.query("INSERT INTO user (fullname, name, email, password, confirmpassword) VALUES (?, ?, ?, MD5(?), MD5(?));", [fullname, name, email, password, confirmpassword], (err, results) => {
-            console.log(err)
-            res.send(results)
-            
+        db.query("SELECT * From user WHERE name = ?", name, (err, results) => {
+            if (err) {
+                console.log(err)
+            }
+            if (!results.length) {
+                var transporter = nodemailer.createTransport({
+                    service:'gmail',
+                    auth:{
+                        user:'codingcomnoreply@gmail.com',
+                        pass:'coding123'
+                    }
+                })
+                var mailOption = {
+                    from:'codingcomnoreply@gmail.com',
+                    to:email,
+                    subject:'Register Successfully',
+                    text:'Welcome,'+fullname+'Thank you for registering your account, we hope you hava nice day and nice journey to the peak'
+                }
+                transporter.sendMail(mailOption,function(err,info){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        console.log('Email sent:'+ info.response)
+                    }
+                })
+                db.query("INSERT INTO user (fullname, name, email, password, confirmpassword) VALUES (?, ?, ?, MD5(?), MD5(?));", [fullname, name, email, password, confirmpassword], (err, results) => {
+                    console.log(err)
+                    res.send(results)
+                })
+            }else {
+                res.send({ message: "Username already exist" })
+            }
         })
     }
 })
@@ -58,18 +87,18 @@ router.post("/login", (req, res) => {
     var crypto = require('crypto')
     var hash = crypto.createHash('md5').update(password).digest('hex')
 
-    db.query("SELECT * From user WHERE name = ?", name ,(err, results) => {
+    db.query("SELECT * From user WHERE name = ?", name, (err, results) => {
         if (err) {
             console.log(err)
         }
         if (results.length > 0) {
-            if (hash.match(results[0].password)!=null) {
+            if (hash.match(results[0].password) != null) {
                 res.json({ loggedIn: true, name: name })
-                
-            }else if(name.length<=0){
-                res.send({message:"You must fill username"})
-            }else if(password.length<=0){
-                res.send({message:"You must fill password"})
+
+            } else if (name.length <= 0) {
+                res.send({ message: "You must fill username" })
+            } else if (password.length <= 0) {
+                res.send({ message: "You must fill password" })
             }
             // else if (password.length <= 8) {
             //     res.json({ loggedIn: true, message: "Password at least has 8 character" })
@@ -84,11 +113,11 @@ router.post("/login", (req, res) => {
             // }
             else {
                 res.json({ loggedIn: false, message: "Wrong Username / Password" })
-                
+
             }
         } else {
             res.json({ loggedIn: false, message: "Wrong Username / Password" })
-            
+
         }
     })
 })
