d5ef174c5c3125645f21c3bdb936db2243a7fc31
rafaelcahya <cahyaputraugira95@gmail.com>
2021-05-03 17:20:17 +0700

    update

--- frontend/src/bootcamp/BootcampHome.jsx
+++ frontend/src/bootcamp/BootcampHome.jsx
@@ -22,7 +22,7 @@ function BootcampHome() {
     }, 500)
     return (
         <>
-            <nav className="hidden fixed w-full bg-white lg:flex justify-between items-center px-16 xl:px-32 py-5">
+            <nav className="bootcamp-navbar hidden fixed w-full bg-white lg:flex justify-between items-center px-16 xl:px-32 py-5">
                 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
                 width="30" height="45" viewBox="0 0 44 59"
                 preserveAspectRatio="xMidYMid meet">
@@ -37,6 +37,7 @@ function BootcampHome() {
                 </g>
                 </svg>
                 <div className="flex gap-14">
+                    <a href="#about" className="hover:text-yellow-500">About</a>
                     <a href="#benefit">Benefit</a>
                     <a href="#program">Program</a>
                     <a href="#pricing">Pricing</a>
@@ -48,7 +49,7 @@ function BootcampHome() {
                 </div>
             </nav>
 
-            <nav className="fixed bg-white w-full flex lg:hidden justify-between items-center px-10 py-3 z-10">
+            <nav className="bootcamp-navbar fixed bg-white w-full flex lg:hidden justify-between items-center px-10 py-3 z-10">
                 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
                 width="30" height="45" viewBox="0 0 44 59"
                 preserveAspectRatio="xMidYMid meet">
@@ -67,7 +68,7 @@ function BootcampHome() {
             <motion.div
             initial= {false}
             variants={menuVariants}
-            animate={open ? "opened" : "closed"} className="bg-white fixed top-0 left-0 w-full lg:hidden flex flex-col">
+            animate={open ? "opened" : "closed"} className="bootcamp-navbar fixed top-0 left-0 w-full lg:hidden flex flex-col">
                 <a className="px-10 py-4 hover:bg-yellow-500 hover:text-white" href="#benefit">Benefit</a>
                 <a className="px-10 py-4 hover:bg-yellow-500 hover:text-white" href="#program">Program</a>
                 <a className="px-10 py-4 hover:bg-yellow-500 hover:text-white" href="#pricing">Pricing</a>
@@ -82,6 +83,12 @@ function BootcampHome() {
                     <p className="bg-yellow-600 px-8 py-3 mt-5 mb-10 rounded-xl">Register Now</p>
                 </div>
             </header>
+
+            <section className="mx-8 md:mx-16 lg:mx-40 py-20 md:py-28" id="about">
+                <p className="text-center text-3xl font-semibold mb-10">What is Fulltime Coding Bootcamp?</p>
+                <p className="text-xl leading-10 font-medium text-center lg:mx-10 xl:mx-32">FullTime Coding Bootcamp is a bootcamp with 11 weeks of intensive training on fullstack web programming for anyone who desires to create impact through technology. This program is open to all and a maximum of 1 year of work experience. Participants will gain experience building products in accordance with real projects.</p>
+
+            </section>
 
             <section className="mx-8 md:mx-16 lg:mx-40 py-20 md:py-28" id="benefit">
                 <p className="text-center text-3xl font-semibold mb-20">Benefits</p>
@@ -113,12 +120,35 @@ function BootcampHome() {
                 </div>
             </section>
 
-            <section className="mx-8 md:mx-16 lg:mx-40 pt-20 md:pt-28 pb-10" id="program">
+            <section className="mx-8 md:mx-16 lg:mx-40 py-20 md:py-28" id="program">
                 <p className="text-center text-3xl font-semibold mb-10">Fulltime Coding Bootcamp Program</p>
-                <p className="text-center lg:mx-10 xl:mx-40">Students will take part in intensive learning starting from the Preparation Phase to learn programming fundamentals and the Bootcamp Phase to learn the hard skills and soft skills needed to become a programmer.</p>
+                <p className="text-xl leading-10 font-medium text-center lg:mx-10 xl:mx-32">Students will take part in intensive learning starting from the Preparation Phase to learn programming fundamentals and the Bootcamp Phase to learn the hard skills and soft skills needed to become a programmer.</p>
             </section>
 
-            <section className="mx-8 md:mx-16 lg:mx-40 py-10">
+            <section className="mx-8 md:mx-16 lg:mx-40 py-20 md:py-28">
+                <p className="text-center text-3xl font-semibold mb-10">Bootcamp Process</p>
+                <div className="flex flex-wrap justify-center items-center gap-10">
+                    <p>Register</p>
+                    <svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0)"><path d="M1.17578 8.5L24.8244 8.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path d="M20.0947 1.5L24.8245 8.5L20.0947 15.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></g><defs>
+                    <clipPath id="clip0"><rect width="16" height="25" fill="white" transform="translate(0.5 16.5) rotate(-90)"/></clipPath></defs></svg>
+                    <p>Document Screening</p>
+                    <svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0)"><path d="M1.17578 8.5L24.8244 8.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path d="M20.0947 1.5L24.8245 8.5L20.0947 15.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></g><defs>
+                    <clipPath id="clip0"><rect width="16" height="25" fill="white" transform="translate(0.5 16.5) rotate(-90)"/></clipPath></defs></svg>
+                    <p>Announcement</p>
+                    <svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0)"><path d="M1.17578 8.5L24.8244 8.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path d="M20.0947 1.5L24.8245 8.5L20.0947 15.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></g><defs>
+                    <clipPath id="clip0"><rect width="16" height="25" fill="white" transform="translate(0.5 16.5) rotate(-90)"/></clipPath></defs></svg>
+                    <p>Bootcamp</p>
+                    <svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0)"><path d="M1.17578 8.5L24.8244 8.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path d="M20.0947 1.5L24.8245 8.5L20.0947 15.5" stroke="#D97706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></g><defs>
+                    <clipPath id="clip0"><rect width="16" height="25" fill="white" transform="translate(0.5 16.5) rotate(-90)"/></clipPath></defs></svg>
+                    <p>Graduation</p>
+                </div>
+            </section>
+
+            <section className="py-20 md:py-28">
                 <p className="text-center text-3xl font-semibold mb-10">Stages</p>
                 <div className="flex flex-col items-center gap-7 text-center">
                     <StageComp
@@ -168,19 +198,29 @@ function BootcampHome() {
                 </div>
             </section>
 
-            <section className="py-10">
-                <div className="grid grid-cols-1 md:grid-cols-3 text-center gap-20 md:gap-0 px-8 md:px-16 lg:px-40 py-10 bg-yellow-200 ">
+            <section className="py-10 mt-10 mb-20 md:mb-28 bg-yellow-200 text-black">
+                <div className="flex justify-center gap-10  lg:mx-10 xl:mx-32">
+                    <div className="text-center px-10 py-5 rounded-xl">
+                        <p>Batch 1 will run on </p>
+                        <p className="text-lg font-semibold mt-2">1 August 2020 - 15 October 2020</p>
+                    </div>
+                    <div className="text-center px-10 py-5 rounded-xl">
+                        <p>Batch 2 will run on </p>
+                        <p className="text-lg font-semibold mt-2">25 October 2020 - 21 Januari 2021</p>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 text-center gap-20 md:gap-0 px-8 md:px-16 lg:px-40 py-10">
                     <div>
-                        <p className="font-semibold text-lg mb-2">Duration</p>
-                        <p>11 weeks</p>
+                        <p className="mb-2">Duration</p>
+                        <p className="font-semibold text-lg ">11 weeks</p>
                     </div>
                     <div>
-                        <p className="font-semibold text-lg mb-2">Day</p>
-                        <p>Monday - Friday</p>
+                        <p className="mb-2">Day</p>
+                        <p className="font-semibold text-lg ">Monday - Friday</p>
                     </div>
                     <div>
-                        <p className="font-semibold text-lg mb-2">Time</p>
-                        <p>09:00 AM - 15:00 PM</p>
+                        <p className="mb-2">Time</p>
+                        <p className="font-semibold text-lg ">09:00 AM - 15:00 PM</p>
                     </div>
                 </div>
             </section>
@@ -188,9 +228,9 @@ function BootcampHome() {
             <section className="mx-8 md:mx-16 lg:mx-40 py-20 md:py-28" id="pricing">
                 <p className="text-center text-3xl font-semibold mb-10">Package Price</p>
                 <div className="flex justify-center">
-                    <div className="flex flex-col items-center text-center bg-white ring-2 ring-yellow-500  w-64 py-5 rounded-xl">
+                    <div className="bootcamp-package flex flex-col items-center text-center ring-2 ring-yellow-500  w-64 py-5 rounded-xl">
                         <p className="text-gray-400 font-semibold text-xs uppercase">Fulltime</p>
-                        <p className="text-yellow-500 font-bold text-4xl my-5">20 juta</p>
+                        <p className="text-yellow-500 font-bold text-4xl my-5">2 juta</p>
                         <div className="flex flex-col gap-2 font-medium text-sm">
                             <p>Intensive training</p>
                             <p>Latest curriculum</p>
@@ -203,7 +243,7 @@ function BootcampHome() {
                 </div>
             </section>
             
-            <footer className="bg-black text-white flex flex-col items-center mt-20 lg:mt-56 py-10">
+            <footer className="bg-black text-white flex flex-col items-center mt-20 lg:mt-56 py-20 md:py-28">
                 <div>
                     <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
                         width="30" height="45" viewBox="0 0 44 59"
