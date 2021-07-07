296e80844399f7633481af3db4503506c2f75bd7
rafaelcahya <cahyaputraugira95@gmail.com>
2021-07-07 17:33:04 +0700

    update

--- frontend/src/components/pages/tutorial/TopicDetail.jsx
+++ frontend/src/components/pages/tutorial/TopicDetail.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable eqeqeq */
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import { Link } from 'react-router-dom'
+import Footer from '../../major/Footer'
+import ShowMoreText from 'react-show-more-text';
 
 import NavbarLogin from '../../major/NavbarLogin'
 import NavbarMobile from '../../major/NavbarMobile'
@@ -30,6 +33,7 @@ function TopicDetail(props) {
     const [valueList, setValueList] = useState([])
     const [rating, setRating] = useState([])
     const [valueAVG, setValueAVG] = useState([])
+    let image = require('../../../asset/upload/'+ localStorage.getItem("image"))
 
     useEffect(() => {
         Axios.get("http://localhost:3001/rating/ratingList/"+ urlid).then((response) => {
@@ -79,75 +83,90 @@ function TopicDetail(props) {
                                 </div>
                                 <div className="flex gap-10 my-10 w-full">
                                     <div className="content-course w-2/3">
-                                        <div dangerouslySetInnerHTML={{ __html: val.about }} />
-                                        <div className="my-10 border-b">
-                                            <p className="text-xl font-semibold mb-2">Reviews</p>
+                                        <div dangerouslySetInnerHTML={{ __html: val.about }} className="tinymce-topic-detail"/>
+                                        <div className="pt-10 border-b">
+                                            <p className="text-xl font-semibold">Reviews</p>
                                             </div>
                                             {
                                                 rating.map(
                                                     (val) => {
-                                                        return <div className="border-b border-gray-300">
-                                                            <div className="flex justify-between gap-2">
-                                                                <p className="text-lg font-semibold">{val.name}</p>
-                                                                <div className="text-right">
-                                                                    {val.rating == 1 ? (<p>😰 {val.rating}</p>) :
-                                                                        val.rating == 2 ? (<p>☹️ {val.rating}</p>) :
-                                                                            val.rating == 3 ? (<p>😑 {val.rating}</p>) :
-                                                                                val.rating == 4 ? (<p>🤩 {val.rating}</p>) :
-                                                                                    (<p>😍 {val.rating}</p>)
+                                                        return <div className="review py-5">
+                                                            <div className="flex justify-between items-center">
+                                                                <div className="flex gap-4">
+                                                                    <img src={image.default} className="ring-1 rounded-full p-0.5 w-10 h-10" alt="Image Profile"/>
+                                                                    <div>
+                                                                        <p className="font-medium">{val.name}</p>
+                                                                        <p className="text-xs text-gray-400">{formatDate(val.ratingCreateAt)}</p>
+                                                                        <p className="py-2"> {val.description}</p>
+                                                                    </div>
+                                                                </div>
+                                                                <div>
+                                                                    {val.rating == 1 ? (<p className="text-2xl">😰</p>) :
+                                                                        val.rating == 2 ? (<p className="text-2xl">☹️</p>) :
+                                                                            val.rating == 3 ? (<p className="text-2xl">😑</p>) :
+                                                                                val.rating == 4 ? (<p className="text-2xl">🤩</p>) :
+                                                                                    (<p className="text-2xl">😍</p>)
                                                                     }
                                                                 </div>
                                                             </div>
-                                                            <p className="text-sm text-gray-600">{formatDate(val.ratingCreateAt)} {formatTime(val.ratingCreateAt)}</p>
-                                                            <p className="py-5"> {val.description}</p>
                                                         </div>
                                                     }
                                                 )
                                             }
                                     </div>
-                                    <div className="sticky self-start top-10 w-1/3">
-                                        {/* <p className="text-2xl lg:text-5xl font-semibold">{val.topikTitle}</p>
-                                        <p className="text-gray-400 text-sm mt-2">{val.topikInfo}</p> */}
-                                        {
-                                            !value.length ? (<p>gkada</p>) : value.map((item)=>{
-                                                return <Link to={"/"+ GenerateID(1,10) +"/" + item.number + "-" + item.id + "/" + val.topikId}>
-                                                <p>MASUK KE COURSE</p>
-                                                </Link>
-                                            })
-                                            
-                                        }
-                                        {/* {
-                                            value.map(
-                                                (item) => {
-                                                    return <div className="flex justify-between items-center">
-                                                        <Link to={"/"+ item.judul +"/" + item.id + "/" + val.topikId}>{item.number}.{item.judul}</Link>
-                                                        <p className="hidden text-xs bg-gray-200 text-gray-500 py-1 px-2 rounded-md">{item.time} min</p>
-                                                    </div>
-                                                }
-                                            )
-					                    } */}
-                                        {
-                                            valueAVG.map(
-                                                (val) => {
-                                                    return <div className="my-10 text-center">
-                                                        {
-                                                            valueList.map(
-                                                                (val) => {
-                                                                    return <p className="text-sm">Your rating for {val.topikTitle}</p>
-                                                                }
-                                                            )
-                                                        }
-                                                        {val.AverageRating == 5 ? (<div className="flex flex-col gap-1"><p className="text-3xl pt-5">😍</p><p className="font-semibold">Very good</p></div>) :
-                                                            val.AverageRating == 4 ? (<div className="flex flex-col gap-1"><p className="text-3xl pt-5">🤩</p><p className="font-semibold">Good</p></div>) :
-                                                                val.AverageRating == 3 ? (<div className="flex flex-col gap-1"><p className="text-3xl pt-5">😑</p><p className="font-semibold">Fair</p></div>) :
-                                                                    val.AverageRating == 2 ? (<div className="flex flex-col gap-1"><p className="text-3xl pt-5">☹️</p><p className="font-semibold">Poor</p></div>) :
-                                                                        val.AverageRating == 1 ? (<div className="flex flex-col gap-1"><p className="text-3xl pt-5">😰</p><p className="font-semibold">Very bad</p></div>) :
-                                                                            (<p className="text-lg font-semibold mb-2">not rated yet</p>)
-                                                        }
-                                                    </div>
-                                                }
-                                            )
-                                        }
+                                    <div className='sticky self-start top-10 w-1/3'>
+                                        <div className="p-5 rounded-lg shadow-lg">
+                                            <div>
+                                                <p className="text-2xl font-semibold mb-4">{val.topikTitle}</p>
+                                                <ShowMoreText
+                                                    more='Read more'
+                                                    less='Read less'
+                                                    anchorClass='anchor-showmore'
+                                                    className="text-sm text-gray-500">
+                                                    <p className="max-w-3xl text-gray-400 text-sm">{val.topikInfo}</p>
+                                                </ShowMoreText>
+                                            </div>
+                                            {
+                                                !value.length ? (<p>gkada</p>) : value.map((item)=>{
+                                                    return <Link to={"/"+ GenerateID(1,10) +"/" + item.number + "-" + item.id + "/" + val.topikId}>
+                                                    <p className="bg-blue-1 text-white font-medium text-center rounded-lg p-2 my-5">Start learning</p>
+                                                    </Link>
+                                                })
+                                            }
+                                        </div>
+                                        <div className="p-5 flex flex-col items-center rounded-lg shadow-lg">
+                                            {
+                                                valueAVG.map(
+                                                    (val) => {
+                                                        return <div>
+                                                            {
+                                                                valueList.map(
+                                                                    (val) => {
+                                                                        return <p className="font-semibold">Average rating</p>
+                                                                    }
+                                                                )
+                                                            }
+                                                            {val.AverageRating == 5 ? (<div className="flex flex-col items-center gap-1"><p className="text-3xl pt-5">😍</p><p className="font-semibold">Very good</p></div>) :
+                                                                val.AverageRating == 4 ? (<div className="flex flex-col items-center gap-1"><p className="text-3xl pt-5">🤩</p><p className="font-semibold">Good</p></div>) :
+                                                                    val.AverageRating == 3 ? (<div className="flex flex-col items-center gap-1"><p className="text-3xl pt-5">😑</p><p className="font-semibold">Fair</p></div>) :
+                                                                        val.AverageRating == 2 ? (<div className="flex flex-col items-center gap-1"><p className="text-3xl pt-5">☹️</p><p className="font-semibold">Poor</p></div>) :
+                                                                            val.AverageRating == 1 ? (<div className="flex flex-col items-center gap-1"><p className="text-3xl pt-5">😰</p><p className="font-semibold">Very bad</p></div>) :
+                                                                                (<p className="text-lg font-semibold mb-2">not rated yet</p>)
+                                                            }
+                                                            <div>
+                                                                Total berapa orng yang ngerating
+                                                            </div>
+                                                            <div>
+                                                                Total berapa orng yang ngerating bad 
+                                                            </div>
+                                                            <div>
+                                                                Total berapa orng yang ngerating very bad
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                )
+                                            }
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -155,6 +174,7 @@ function TopicDetail(props) {
                     )
                 }
             </div>
+            <Footer/>
         </>
     )
 }
